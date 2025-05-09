25f30356c3d19c95a36f507c7b90272a33c77ea4
Anna <annakirillova16032@gmail.com>
2025-05-09 16:03:30 +0300

    2

--- lidea/lidea/settings.py
+++ lidea/lidea/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,8 @@ SECRET_KEY = "django-insecure-6_)i+n%98^e(h9%qq^@owdaxbjt15%d-@+m@gb47v#_+zybxdc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
