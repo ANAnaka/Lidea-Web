37627296fd12e623a23701d17f2d8ec6f1d5403c
Anna <annakirillova16032@gmail.com>
2025-05-09 17:42:49 +0300

    5

--- lidea/lidea/firebase.py
+++ lidea/lidea/firebase.py
@@ -1,9 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import os
 
-# Путь к JSON-файлу
-cred = credentials.Certificate("C:/Users/annak/VS/Lidea/lidea/lidea/credentials/lidea-db.json")
-
+# Путь к JSON-файлу (относительный путь)
+cred_path = os.path.join(os.path.dirname(__file__), 'credentials', 'lidea-db.json')
+cred = credentials.Certificate(cred_path)
 
 # URL вашей Realtime Database
 firebase_admin.initialize_app(cred, {
